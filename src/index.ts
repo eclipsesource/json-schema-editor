eda6bcb2579902833641652914794b71e81ac17b
Jiping Xu <xujiping2003@gmail.com>
2016-11-29 11:29:25 +0100

    import angular-ui-tree into index

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,10 @@ import "angular";
 // load our default (non specific) css
 import "font-awesome/css/font-awesome.css";
 import "bootstrap/dist/css/bootstrap.css";
-import "./styles/screen.scss";
+import "angular-ui-tree/dist/angular-ui-tree.js"
+import "angular-ui-tree/dist/angular-ui-tree.min.css"
 
-angular.module("app", ["app.application"]);
+angular.module("app", ["app.application", "ui.tree"]);
 angular.bootstrap(document, ["app"], {
     strictDi: true
 });

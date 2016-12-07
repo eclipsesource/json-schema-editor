38f4678bd1dec97851bd7262aa1eeb2223604f55
Jiping Xu <xujiping2003@gmail.com>
2016-12-07 21:02:46 +0100

    integrate jsonforms

--- src/modules/application/angular/index.ts
+++ src/modules/application/angular/index.ts
@@ -2,6 +2,8 @@ import "angular";
 import "angular-ui-tree/dist/angular-ui-tree.js";
 import "angular-ui-tree/dist/angular-ui-tree.min.css";
 import "angular-ui-tree/examples/css/app.css";
+import "jsonforms/dist/jsonforms.css";
+require('jsonforms');
 
 import {TreeMasterDetailComponent} from "./components/tree-master-detail-panel/TreeMasterDetailComponent";
 import {PropertiesComponent} from "./components/properties-panel/PropertiesComponent";
@@ -10,7 +12,7 @@ import {ItemComponent} from "./components/item-panel/ItemComponent";
 import {HeaderComponent} from "./components/header/HeaderComponent";
 import {Parser} from "./components/parser/Parser";
 
-angular.module("app.application", ["ui.tree"])
+angular.module("app.application", ["ui.tree", 'jsonforms'])
     .component("jsonSchemaEditorApplication", new JsonSchemaEditorApplicationComponent())
     .component("headerComponent", new HeaderComponent())
     .component("itemComponent", new ItemComponent())

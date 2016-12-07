316d36ce5c47075923dde36a053d823b10c55c51
Jiping Xu <xujiping2003@gmail.com>
2016-12-07 21:05:20 +0100

    bind schema and data of jsonforms with data passed from parent

--- src/modules/application/angular/components/jsonSchemaEditorApplication/JsonSchemaEditorApplicationComponent.ts
+++ src/modules/application/angular/components/jsonSchemaEditorApplication/JsonSchemaEditorApplicationComponent.ts
@@ -1,3 +1,13 @@
 export class JsonSchemaEditorApplicationComponent implements ng.IComponentOptions{
-    public template : string = require('./jsonSchemaEditorAppComponent.html')
+    public template : string = require('./jsonSchemaEditorAppComponent.html');
+    public controller : Function = JsonSchemaEditorApplicationController;
+}
+
+export class JsonSchemaEditorApplicationController{
+    public schema;
+    public data = "";
+    public selectElement(schema, data){
+        this.schema = schema;
+        this.data = data;
+    }
 }

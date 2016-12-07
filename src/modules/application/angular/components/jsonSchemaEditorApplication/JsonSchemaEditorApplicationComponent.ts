71743e87cb2a8b96ab8db33436ae9d7bc3fba43d
Jiping Xu <xujiping2003@gmail.com>
2016-12-07 21:17:34 +0100

    Revert "bind schema and data of jsonforms with data passed from parent"
    This reverts commit 316d36ce5c47075923dde36a053d823b10c55c51.

--- src/modules/application/angular/components/jsonSchemaEditorApplication/JsonSchemaEditorApplicationComponent.ts
+++ src/modules/application/angular/components/jsonSchemaEditorApplication/JsonSchemaEditorApplicationComponent.ts
@@ -1,13 +1,3 @@
 export class JsonSchemaEditorApplicationComponent implements ng.IComponentOptions{
-    public template : string = require('./jsonSchemaEditorAppComponent.html');
-    public controller : Function = JsonSchemaEditorApplicationController;
-}
-
-export class JsonSchemaEditorApplicationController{
-    public schema;
-    public data = "";
-    public selectElement(schema, data){
-        this.schema = schema;
-        this.data = data;
-    }
+    public template : string = require('./jsonSchemaEditorAppComponent.html')
 }
--- src/modules/application/angular/components/tree-master-detail-panel/TreeMasterDetailComponent.ts
+++ src/modules/application/angular/components/tree-master-detail-panel/TreeMasterDetailComponent.ts
@@ -1,72 +1,7 @@
 export class TreeMasterDetailComponent implements ng.IComponentOptions {
-    public controller : Function = TreeMasterDetailController;
-    public template : string = require('./TreeMasterDetailComponent.html');
-    public bindings : any = {
-        onSelectElement: '&'
-    };
+    public controller: Function = TreeMasterDetailController;
+    public template : string = require('./TreeMasterDetailComponent.html')
 }
 export class TreeMasterDetailController {
-    public selectElement(){
-        this.onSelectElement({schema: this.schema, data: this.data});
-    }
-    public schema = {
-      "type": "object",
-      "properties": {
-        "name": {
-          "type": "string"
-        },
-        "classes": {
-          "type": "array",
-          "items": {
-            "type": "object",
-            "properties": {
-              "name": {
-                "type": "string"
-              },
-              "attributes": {
-                "type": "array",
-                "items": {
-                  "type": "object",
-                  "properties": {
-                    "name": {
-                      "type": "string"
-                    },
-                    "visibility": {
-                      "type": "string",
-                      "enum": ["Public", "Private","Protected"]
-                    },
-                    "type": {
-                      "type": "string",
-                      "enum": ["Boolean","Integer","Real","String"]
-                    }
-                  }
-                }
-              }
-            }
-          }
-        }
-      }
-    };
-    public data={
-      "name": "Domain Model Example",
-      "classes": [
-        {
-          "name": "Person",
-          "attributes": [
-            {"name": "firstName", "visibility": "Public", "type": "String"},
-            {"name": "lastName", "visibility": "Public", "type": "String"},
-            {"name": "age", "visibility": "Private","type": "Number"}
-          ]
-        },
-        {
-          "name": "Project",
-          "attributes": [
-            {"name": "id", "visibility": "Private", "type": "String"},
-            {"name": "name", "visibility": "Public", "type": "String"},
-            {"name": "description", "visibility": "Public","type": "String"}
-          ]
-        }
-      ]
-    };
 
 }

106e877fd868603871d5953847bda863cc231cf0
Jiping Xu <xujiping2003@gmail.com>
2017-02-09 20:41:49 +0100

    remove -panel from properties directory name

--- src/app/index.ts
+++ src/app/index.ts
@@ -12,12 +12,12 @@ import "angular-aria/angular-aria.js";
 import "angular-sanitize/angular-sanitize.js";
 require('jsonforms');
 
-import {TreeComponent} from "./components/tree/TreeComponent";
-import {PropertiesComponent} from "./components/properties-panel/PropertiesComponent";
+import {TreeComponent} from "./components/tree/treeComponent";
+import {PropertiesComponent} from "./components/properties/propertiesComponent";
 import {JsonSchemaEditorApplicationComponent} from "./components/jsonSchemaEditorApplication/JsonSchemaEditorApplicationComponent";
 import {PaletteComponent} from "./components/palette/paletteComponent";
-import {HeaderComponent} from "./components/header/HeaderComponent";
-import {Parser} from "./components/parser/Parser";
+import {HeaderComponent} from "./components/header/headerComponent";
+import {Parser} from "./components/parser/parser";
 
 angular.module("app.application", ['ui.tree', 'jsonforms','ui.bootstrap','ngAnimate','ngAria','ngMaterial','ngSanitize'])
     .component("jsonSchemaEditorApplication", new JsonSchemaEditorApplicationComponent())
